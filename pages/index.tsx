2093bb3bdf87ea7369614db1edfc2964b4d89a6c
Emanuel Lorenzo <aemalorenzo@gmail.com>
2020-10-19 17:29:50 -0300

    app tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const Home: React.FC = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  </div>
+);
+
+export default Home;

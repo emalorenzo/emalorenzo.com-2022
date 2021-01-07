29cc3f16975c14756a62b70f0d5f7642144087a0
Emanuel Lorenzo <aemalorenzo@gmail.com>
2021-01-07 01:34:18 -0300

    Fallback component

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,4 @@ export { Layout } from './layout';
 export { PreviewAlert } from './previewAlert';
 export { PostCard } from './postCard';
 export { Date } from './date';
+export { Fallback } from './fallback';

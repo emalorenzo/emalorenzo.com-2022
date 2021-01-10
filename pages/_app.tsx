82c039f7c11fd9ea18a5dcac9cc6e515fd923189
Ema Lorenzo <aemalorenzo@gmail.com>
2021-01-10 02:11:17 -0300

    header provider integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AnimateSharedLayout } from 'framer-motion';
+
 import { Layout } from 'components';
+import { HeaderProvider } from 'context';
 import { theme } from 'theme';
 
 import '../styles/globals.css';
@@ -9,11 +11,14 @@ import '../styles/globals.css';
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <AnimateSharedLayout>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AnimateSharedLayout>
+      {/* shares blog / snippet / history title to header */}
+      <HeaderProvider>
+        <AnimateSharedLayout>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </AnimateSharedLayout>
+      </HeaderProvider>
     </ChakraProvider>
   );
 };

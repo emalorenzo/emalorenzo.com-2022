4ac5b9967e1c33b9ecefd69ad22413c20ae35857
Ema Lorenzo <aemalorenzo@gmail.com>
2022-03-15 00:27:40 -0300

    fix padding

--- src/layouts/BlogPost/BlogPostLayout.tsx
+++ src/layouts/BlogPost/BlogPostLayout.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export const BlogPostLayout = ({ post, preview }) => {
   const { content, title, categories } = post;
   return (
-    <main style={{ marginTop: '12rem', padding: '0 2      0rem' }}>
+    <main style={{ marginTop: '12rem', padding: '0 20rem' }}>
       <motion.article
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}

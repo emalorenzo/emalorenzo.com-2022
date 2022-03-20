be01594497def1fe1849dccb4c1cf6eaf378a711
Ema Lorenzo <aemalorenzo@gmail.com>
2022-03-19 21:44:06 -0300

    error pages

---
+++ src/pages/500.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  height: 100%;
+  display: grid;
+  place-items: center;
+  align-content: center;
+  gap: 1rem;
+
+  a {
+    color: var(--text-color);
+  }
+`;
+
+const ErrorPage: NextPage = () => {
+  return (
+    <Wrapper>
+      <Head>
+        <title>Ema Lorenzo</title>
+      </Head>
+
+      <h1>There is no content here</h1>
+      <Link href="/" passHref>
+        <a>Go back to home</a>
+      </Link>
+    </Wrapper>
+  );
+};
+
+export default ErrorPage;
